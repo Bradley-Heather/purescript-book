47ff2e99a129f2f8897e260970851bc43dd19493
Bradley Heather <bradleymheather@gmail.com>
2021-10-19 12:44:32 -0400

    Update Main.purs

--- exercises/chapter5/test/Main.purs
+++ exercises/chapter5/test/Main.purs
@@ -7,7 +7,7 @@ import ChapterExamples (Amp(..), current, fromString, gcd, gcdV2, isEmpty, lives
 import Data.Int (round)
 import Data.Maybe (Maybe(Just, Nothing))
 import Data.Person (Person)
-import Data.Picture (Shape(..), Picture, getCenter, origin)
+import Data.Picture (Shape(..), Picture, getCenter, origin, shapeBounds)
 import Effect (Effect)
 import Test.Unit (TestSuite, suite, test)
 import Test.Unit.Assert as Assert
@@ -33,7 +33,6 @@ main :: Effect Unit
 main =
   runTest do
     runChapterExamples
-    {-  Move this block comment starting point to enable more tests
     suite "Exercise Group - Simple Pattern Matching" do
       test "Exercise - factorial" do
         Assert.equal 1
@@ -43,7 +42,7 @@ main =
         Assert.equal 24
           $ factorial 4
         Assert.equal 3628800
-          $ factorial 10
+          $ factorial 10  
       test "Exercise - binomial" do
         Assert.equal 1
           $ binomial 10 0
@@ -54,7 +53,7 @@ main =
         Assert.equal 252
           $ binomial 10 5
         Assert.equal 1
-          $ binomial 5 5
+          $ binomial 5 5 
       test "Exercise - pascal" do
         Assert.equal 1
           $ pascal 10 0
@@ -65,24 +64,24 @@ main =
         Assert.equal 252
           $ pascal 10 5
         Assert.equal 1
-          $ pascal 5 5
+          $ pascal 5 5 
     suite "Exercise Group - Array and Record Patterns" do
       test "Exercise - sameCity" do
         Assert.equal true
           $ sameCity john rose
         Assert.equal false
-          $ sameCity amy rose
+          $ sameCity amy rose 
       test "Exercise - fromSingleton" do
         Assert.equal "default"
           $ fromSingleton "default" []
         Assert.equal "B"
           $ fromSingleton "default" ["B"]
         Assert.equal "default"
-          $ fromSingleton "default" ["B", "C", "D"]
+          $ fromSingleton "default" ["B", "C", "D"] 
     suite "Exercise Group - Algebraic Data Types" do
       test "Exercise - circleAtOrigin" do
         Assert.equal origin
-          $ getCenter circleAtOrigin
+          $ getCenter circleAtOrigin 
       test "Exercise - doubleScaleAndCenter" do
         Assert.equal (Circle origin 10.0)
           $ doubleScaleAndCenter $ Circle origin 5.0
@@ -97,8 +96,8 @@ main =
         Assert.equal (Line { x: -4.0, y: -4.0 } { x: 4.0, y: 4.0 })
           $ doubleScaleAndCenter $ Line { x: 0.0, y: 4.0 } { x: 4.0, y: 8.0 }
         Assert.equal (Text { x: 0.0, y: 0.0 } "Hello .purs!" )
-          $ doubleScaleAndCenter $ Text { x: 4.0, y: 6.0 } "Hello .purs!"
-      test "Exercise - shapeText" do
+          $ doubleScaleAndCenter $ Text { x: 4.0, y: 6.0 } "Hello .purs!" 
+      test "Exercise - shapeText" do 
         Assert.equal (Just "Hello .purs!")
           $ shapeText $ Text origin "Hello .purs!"
         Assert.equal Nothing
@@ -106,12 +105,12 @@ main =
         Assert.equal Nothing
           $ shapeText $ Rectangle origin 1.0 1.0
         Assert.equal Nothing
-          $ shapeText $ Line origin { x: 1.0, y: 1.0 }
+          $ shapeText $ Line origin { x: 1.0, y: 1.0 } 
     suite "Exercise Group - Newtype" do
       test "Exercise - calculateWattage" do
         Assert.equal 60.0
           $ let (Watt w) = calculateWattage (Amp 0.5) (Volt 120.0)
-            in w
+            in w 
     suite "Exercise Group - Vector Graphics" do
       test "Exercise - area" do
         Assert.equal 50
@@ -122,7 +121,7 @@ main =
           $ round $ area $ Line origin { x: 2.0, y: 2.0 }
         Assert.equal 0
           $ round $ area $ Text origin "Text has no area!"
-      test "Exercise - Clipped shapeBounds" do
+      test "Exercise - Clipped shapeBounds" do 
         Assert.equal { top: -2.0, left: -2.0, right: 2.0, bottom: 2.0 }
           -- Note to users: You'll need to manually import shapeBounds
           -- from Data.Picture. Don't import from Test.NoPeeking.Solutions.
@@ -130,7 +129,6 @@ main =
         Assert.equal { top: 3.0, left: 3.0, right: 7.0, bottom: 7.0 }
           $ shapeBounds (Clipped samplePicture { x: 5.0, y: 5.0 } 4.0 4.0)
 
--}
 runChapterExamples :: TestSuite
 runChapterExamples =
   suite "Chapter Examples" do
