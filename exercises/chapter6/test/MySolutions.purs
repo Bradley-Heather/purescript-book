3748bc3237431715869855e1d47a0ce9fce1f5a3
Bradley Heather <bradleymheather@gmail.com>
2021-10-21 13:30:47 -0400

    Update MySolutions.purs

--- exercises/chapter6/test/MySolutions.purs
+++ exercises/chapter6/test/MySolutions.purs
@@ -1,9 +1,12 @@
 module Test.MySolutions where
 
 import Prelude
+import Undefined
 
 import Data.Generic.Rep  (class Generic)
+import Data.Foldable 
 import Data.Show.Generic (genericShow)
+import Data.Array        (nub, nubEq)
 
 newtype Point = Point 
    { x :: Number 
@@ -65,3 +68,49 @@ instance semigroupNonEmpty :: Semigroup (NonEmpty a) where
 instance functorNonEmpty :: Functor NonEmpty where 
      map f (NonEmpty a as) = NonEmpty (f a) (map f as)
 
+-----------------------------
+data Extended a = Infinite | Finite a 
+
+derive instance eqExtended :: Eq a => Eq (Extended a)
+
+instance ordExtended :: Ord a => Ord (Extended a) where 
+    compare Infinite Infinite      = EQ
+    compare Infinite (Finite _)    = GT 
+    compare (Finite _) Infinite    = LT 
+    compare (Finite a) (Finite a') = compare a a'
+
+------------------------------
+instance foldableNonEmpty :: Foldable NonEmpty where
+    foldr f n (NonEmpty a as) = foldr f n ([a] <> as)
+    foldl f n (NonEmpty a as) = foldl f n ([a] <> as) 
+    foldMap f (NonEmpty a as) = foldMap f ([a] <> as)
+
+------------------------------
+data OneMore f a = OneMore a (f a) 
+
+instance foldableOneMore :: Foldable f => Foldable (OneMore f) where 
+    foldr f n (OneMore a fa) = f a (foldr f n fa)
+    foldl f n (OneMore a fa) = foldl f (f n a) fa 
+    foldMap f (OneMore a fa) = f a <> foldMap f fa   
+
+-------------------------------
+
+instance eqShape :: Eq Shape where 
+   eq (Circle p r) (Circle p' r')            = p == p' && r == r'
+   eq (Rectangle p l b) (Rectangle p' l' b') = p == p' && l == l' && b == b' 
+   eq (Line p1 p2) (Line p1' p2')            = p1 == p1' && p2 == p2' 
+   eq (Text p t) (Text p' t')                = p == p' && t == t'
+   eq _ _                                    = false 
+
+instance eqPoint :: Eq Point where
+   eq (Point {x : a, y : b}) (Point {x : a', y : b'}) = a == a' && b == b'
+
+derive instance ordPoint :: Ord Point 
+
+derive instance ordShape :: Ord Shape 
+
+dedupShapes :: Array Shape -> Array Shape 
+dedupShapes = nubEq 
+
+dedupShapesFast :: Array Shape -> Array Shape 
+dedupShapesFast = nub 
